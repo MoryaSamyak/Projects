9dbd186e49bf328066722e26ac04facab2dd1372
Morya Samyak Arrais Benício <ayromkaymas@gmail.com>
2025-03-02 17:45:22 -0300

    Complete code

--- WEB/PHP_train/PHPS/Altura.php
+++ WEB/PHP_train/PHPS/Altura.php
@@ -1,9 +1,21 @@
 <?php 
 
-    $info = readline("Informe a quantidade de brinquedos e a altura de Carlinhos");
+    $info = readline();
     $separation = explode(" ", $info);
 
     $Quant_toys = $separation[0];
     $Height = $separation[1];
 
+    $Toys = readline();
+    $Height_toys = explode(' ', $Toys);
+
+    $count = 0;
+
+    for ($i = 0; $i < $Quant_toys; $i++) {
+
+        if ($Height >= $Height_toys[$i]){
+            $count ++;
+        }
+    }
+    echo $count;
 ?>
