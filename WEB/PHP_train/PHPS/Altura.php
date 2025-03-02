21e5c3f9a0711e57d6383100b86a64c5b2a8c59e
Morya Samyak Arrais Benício <ayromkaymas@gmail.com>
2025-03-02 16:55:57 -0300

    Add input

--- WEB/PHP_train/PHPS/Altura.php
+++ WEB/PHP_train/PHPS/Altura.php
@@ -1,5 +1,9 @@
 <?php 
-    $resultado = 215;
-    $num = 2;
-    $altura = 41;
+
+    $info = readline("Informe a quantidade de brinquedos e a altura de Carlinhos");
+    $separation = explode(" ", $info);
+
+    $Quant_toys = $separation[0];
+    $Height = $separation[1];
+
 ?>
